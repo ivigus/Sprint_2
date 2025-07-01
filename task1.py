18006badd0ff79aa2508e87bb6e97be68d1f56d7
Vadim <iviguss@ya.ru>
2025-07-01 17:11:35 +0300

    task1

--- task1.py
+++ task1.py
@@ -15,16 +15,12 @@ class ExtendedCase(Case):
     def __init__(self, test_case_id, name, step_description, expected_result, precondition, environment):
         super().__init__(test_case_id, name, step_description, expected_result)
         self.precondition = precondition
-        self.enviroment = environment
+        self.environment = environment
         
     def print_test_case_info(self):
-        print(f"ID тест-кейса:  {self.test_case_id}"
-              f"\nНазвание: {self.name}"
-              f"\nОписание шага: {self.step_description}"
-              f"\nОжидаемый результат: {self.expected_result}"
-              f"\nПредусловие: {self.recondition}"
-              f"\nОкружение: {self.environment}"
-              )
+        super().print_test_case_info()
+        print(f"Предусловие: {self.precondition}"
+              f"\nОкружение: {self.environment}")
         
 case = ExtendedCase('1', 'Наличие кнопки Принять', '1. Открыть вкладку приёма документов 2. Проверить наличие кнопки ', 'Кнопка доступна', 'Открыть сервис', 'Яндекс Браузер')
-Case.print_test_case_info(case)
+case.print_test_case_info()
